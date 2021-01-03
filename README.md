f2ca6c9ee36553670497dc18794c443dce8b7099
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-01-03 20:55:14 +0100

    fix checkbox: value -> checked; show current API

--- README.md
+++ README.md
@@ -4,23 +4,23 @@ Chrome Extension of ImmoScout project (scrape, see results and organize them)
 
 ## Getting started
 
-- copy `src/popup/.env-example` into new files: `.env` & `.env.production` (git ignored) and update values accordingly
-- `npm i` (install dependencies)
-- `cd src/popup && npm i` (install dependencies of Popup (React App))
-- `cd ..` (go back)
-- `npm run watch` (observe changes and build in dev mode)
-- In Chrome: Menu > More tools > Extensions, click on "Load unpacked" and select our ./dist folder
-- The Chrome extension should be visible and always reload on code change (& save)
-  - but you have to close the Extensions Popup and re-open it, to see changes inside the Popup
+-   copy `src/popup/.env-example` into new files: `.env` & `.env.production` (git ignored) and update values accordingly
+-   `npm i` (install dependencies)
+-   `cd src/popup && npm i` (install dependencies of Popup (React App))
+-   `cd ..` (go back)
+-   `npm run watch` (observe changes and build in dev mode)
+-   In Chrome: Menu > More tools > Extensions, click on "Load unpacked" and select our ./dist folder
+-   The Chrome extension should be visible and always reload on code change (& save)
+    -   but you have to close the Extensions Popup and re-open it, to see changes inside the Popup
 
 ## Deploy
 
 This Chrome Extension is not in official Chrome Web Store. So "Deploy" is only a local build:
 
-- `npm build`
-- zip /dist folder
-- In Chrome: Menu > More tools > Extensions, click on "Pack extension" and select our zip file
+-   `npm build`
+-   zip /dist folder
+-   In Chrome: Menu > More tools > Extensions, click on "Pack extension" and select our zip file
 
 ## Further Info
 
-- Base Setup inspired by article [Create a Chrome Extension Using React and TypeScript](https://medium.com/better-programming/create-a-chrome-extension-using-react-and-typescript-50e94e14320c)
+-   Base Setup inspired by article [Create a Chrome Extension Using React and TypeScript](https://medium.com/better-programming/create-a-chrome-extension-using-react-and-typescript-50e94e14320c)
--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -4,88 +4,100 @@ import "./App.css";
 import { saveProperty } from "./requests/requests";
 
 function App() {
-  const [scrapeResult, setScrapeResult] = useState<ScrapeResponse>();
-  const [waiting, setWaiting] = useState(false);
-  const [useLocalApi, setUseLocalApi] = useState(false);
-  const [error, setError] = useState("");
+    const [scrapeResult, setScrapeResult] = useState<ScrapeResponse>();
+    const [waiting, setWaiting] = useState(false);
+    const [useLocalApi, setUseLocalApi] = useState(false);
+    const [error, setError] = useState("");
 
-  const onScrapeClick = useCallback(() => {
-    setWaiting(true);
-    // send to ContentScript
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0].id === undefined) throw Error("tabs[0] not defined");
-      chrome.tabs.sendMessage(tabs[0].id, Message.Scrape, onScrapeResult);
-    });
-  }, []);
+    const onScrapeClick = useCallback(() => {
+        setWaiting(true);
+        // send to ContentScript
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (tabs[0].id === undefined) throw Error("tabs[0] not defined");
+            chrome.tabs.sendMessage(tabs[0].id, Message.Scrape, onScrapeResult);
+        });
+    }, []);
 
-  const onScrapeResult = (result: ScrapeResponse) => {
-    setScrapeResult(result);
-    setWaiting(false);
-  };
-
-  const onSaveClick = useCallback(() => {
-    if (!scrapeResult) {
-      console.error("no scrape result");
-      return;
-    }
-    setWaiting(true);
-    saveProperty(scrapeResult, useLocalApi)
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        setScrapeResult(undefined);
+    const onScrapeResult = (result: ScrapeResponse) => {
+        setScrapeResult(result);
         setWaiting(false);
-      })
-      .catch((error: Error) => {
-        console.error(error.message);
-        setError(`saveProperty request: ${error.message}`);
-        setWaiting(false);
-      });
-  }, [scrapeResult]);
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="logo64.png" alt="" />
-        <h1>Scraper</h1>
-      </header>
-      <div>
-          <label htmlFor="useLocalApi">
-              <input type="checkbox" name="useLocalApi" id="useLocalApi" value={useLocalApi} onChange={(e) => setUseLocalApi(e.target.value)}/>
-              local API
-          </label>
-      </div>
-      <div className="App-content">
-        <button
-          type="button"
-          disabled={waiting}
-          onClick={onScrapeClick}
-          className="App-btn"
-        >
-          scrape!
-        </button>
-        {scrapeResult && (
-          <button
-            type="button"
-            disabled={waiting}
-            onClick={onSaveClick}
-            className="App-btn"
-          >
-            save
-          </button>
-        )}
-        {error && <div className="App-error">{error}</div>}
-        <div className="App-scrape-result">
-          {scrapeResult &&
-            Object.entries(scrapeResult).map((entry) => (
-              <p key={entry[0]}>
-                <span className="App-key">{entry[0]}</span>
-                <span className="App-value">{entry[1]}</span>
-              </p>
-            ))}
+    const onSaveClick = useCallback(() => {
+        if (!scrapeResult) {
+            console.error("no scrape result");
+            return;
+        }
+        setWaiting(true);
+        saveProperty(scrapeResult, useLocalApi)
+            .then((response) => {
+                if (!response.ok) throw Error(response.statusText);
+                setScrapeResult(undefined);
+                setWaiting(false);
+            })
+            .catch((error: Error) => {
+                console.error(error.message);
+                setError(`saveProperty request: ${error.message}`);
+                setWaiting(false);
+            });
+    }, [scrapeResult]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="logo64.png" alt="" />
+                <h1>Scraper</h1>
+            </header>
+            <div>
+                <label htmlFor="useLocalApi">
+                    <input
+                        type="checkbox"
+                        name="useLocalApi"
+                        id="useLocalApi"
+                        checked={useLocalApi}
+                        onChange={(e) => setUseLocalApi(e.target.checked)}
+                    />
+                    local API
+                </label>
+                <p>
+                    current API:{" "}
+                    {useLocalApi
+                        ? process.env.REACT_APP_API_ENDPOINT_LOCAL
+                        : process.env.REACT_APP_API_ENDPOINT}
+                </p>
+            </div>
+            <div className="App-content">
+                <button
+                    type="button"
+                    disabled={waiting}
+                    onClick={onScrapeClick}
+                    className="App-btn"
+                >
+                    scrape!
+                </button>
+                {scrapeResult && (
+                    <button
+                        type="button"
+                        disabled={waiting}
+                        onClick={onSaveClick}
+                        className="App-btn"
+                    >
+                        save
+                    </button>
+                )}
+                {error && <div className="App-error">{error}</div>}
+                <div className="App-scrape-result">
+                    {scrapeResult &&
+                        Object.entries(scrapeResult).map((entry) => (
+                            <p key={entry[0]}>
+                                <span className="App-key">{entry[0]}</span>
+                                <span className="App-value">{entry[1]}</span>
+                            </p>
+                        ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
