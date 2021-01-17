814ab5a113820207589919e06fa0f83eaa491bf8
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-01-17 21:46:04 +0100

    only run on immobilienscout24.de & console.log
    - console.log("ImmoScout24 Scraper is connected")

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -2,6 +2,8 @@ import { Message, ScrapeResponse } from "../messages/messages";
 import "./contentscript.scss";
 import { scrape } from "./scrape";
 
+console.log("ImmoScout24 Scraper is connected");
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // got message...
     const fromContentScript = !!sender.tab;
